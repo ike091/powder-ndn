4eaa9b00ad2ad396321aca83ed3d22c7ab042f9c
ike091 <ike091@gmail.com>
2020-07-02 14:06:21 -0600

    Remove link shaping parameters (doesn't work)

--- profile.py
+++ profile.py
@@ -22,43 +22,10 @@ class GLOBALS(object):
 
 
 # define network parameters
-portal.context.defineParameter("latency_external", "Latency to external data network (ms)", portal.ParameterType.STRING, 40)
-portal.context.defineParameter("bandwidth_external", "Bandwidth to external data network (kbps)", portal.ParameterType.STRING, 110000)
-portal.context.defineParameter("packet_loss_external", "Packet loss rate to external data network (rate between 0.0 and 1.0)", portal.ParameterType.STRING, '0.0')
-
-portal.context.defineParameter("latency_internal", "Latency to internal data network (ms)", portal.ParameterType.STRING, 2)
-portal.context.defineParameter("bandwidth_internal", "Bandwidth to internal data network (kbps)", portal.ParameterType.STRING, 110000)
-portal.context.defineParameter("packet_loss_internal", "Packet loss rate to internal data network (rate between 0.0 and 1.0)", portal.ParameterType.STRING, '0.0')
-
 portal.context.defineParameter("physical_host_type", "Type of physical host (d740, d840, or d820)", portal.ParameterType.STRING, 'd740')
 
 # retrieve the values the user specifies during instantiation
 params = portal.context.bindParameters()
-
-# check link shape parameter validity
-try:
-    if int(params.latency_external) < 0 or int(params.latency_internal) < 0:
-        portal.context.reportError(portal.ParameterError("Latency cannot be negative."))
-except ValueError:
-        portal.context.reportError(portal.ParameterError("Error in parsing latency parameter"))
-
-try:
-    if int(params.bandwidth_external) < 0 or int(params.bandwidth_internal) < 0:
-        portal.context.reportError(portal.ParameterError("Bandwidth cannot be negative."))
-except ValueError:
-        portal.context.reportError(portal.ParameterError("Error in parsing bandwidth parameter"))
-
-try:
-    if float(params.packet_loss_external) > 1 or float(params.packet_loss_external) < 0:
-        portal.context.reportError(portal.ParameterError("Packet loss rate must be a number between 0 and 1."))
-except ValueError:
-    portal.context.reportError(portal.ParameterError("Packet loss rate must be a number between 0 and 1."))
-
-try:
-    if float(params.packet_loss_internal) > 1 or float(params.packet_loss_internal) < 0:
-        portal.context.reportError(portal.ParameterError("Packet loss rate must be a number between 0 and 1."))
-except ValueError:
-    portal.context.reportError(portal.ParameterError("Packet loss rate must be a number between 0 and 1."))
 
 # check node type validity
 if params.physical_host_type not in GLOBALS.PHYSICAL_NODE_TYPES:
@@ -147,22 +114,6 @@ for UE in UEs:
 # set up router links
 external_dn_link = request.Link(members=[routers['up-cl'], routers['external-dn']])
 internal_dn_link = request.Link(members=[routers['up-cl'], routers['internal-dn']])
-
-# shape external link
-external_dn_link.bandwidth = int(params.bandwidth_external)
-external_dn_link.latency = int(params.latency_external)
-# no exception catching is needed as validity has already been checked
-external_dn_link.plr = float(params.packet_loss_external) 
-
-# shape internal link
-internal_dn_link.bandwidth = int(params.bandwidth_internal)
-internal_dn_link.latency = int(params.latency_internal)
-internal_dn_link.plr = float(params.packet_loss_internal) 
-
-
-routers['up-cl'].addService(pg.Execute(shell='sh', command='sudo echo "' + str(params.bandwidth_external) + '" >> /var/tmp/test.txt'))
-routers['up-cl'].addService(pg.Execute(shell='sh', command='sudo echo "' + str(params.latency_external) + '" >> /var/tmp/test.txt'))
-routers['up-cl'].addService(pg.Execute(shell='sh', command='sudo echo "' + str(params.packet_loss_internal) + '" >> /var/tmp/test.txt'))
 
 # output request
 pc.printRequestRSpec(request)
