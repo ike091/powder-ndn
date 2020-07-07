c422190de3587bd9d3c9a491e5487aae8d0b93a6
ike091 <ike091@gmail.com>
2020-07-07 13:16:35 -0600

    Update install_ndn_client.sh to install newer version of PyNDN2, exclude .vscode from repo

--- setup/install_ndn_client.sh
+++ setup/install_ndn_client.sh
@@ -5,14 +5,11 @@
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev libffi-dev python3-dev python3-pip -y
 
-# Maintained version of pyndn does not have packet v0.3 support
+# Maintained version of pyndn currently does not have packet v0.3 support, so alternate version must be built
 # sudo pip3 install pyndn
-
 git clone https://github.com/Pesa/PyNDN2 ~/PyNDN2
-cd ~/PyNDN2
-git merge packet03
+cd ~/PyNDN2 && git merge remotes/origin/packet03
 pip3 install ~/PyNDN2
-
 
 # copy the client code to the user's home directory
 cp /local/repository/request_data.py ~/
